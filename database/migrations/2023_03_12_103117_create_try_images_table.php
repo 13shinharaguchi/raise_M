2643e5cd7670e561881af4023d09e0b4b945e386
13shinharaguchi <shin13usa@icloud.com>
2023-03-17 07:14:19 +0000

    create image table

--- database/migrations/2023_03_12_103117_create_try_images_table.php
+++ database/migrations/2023_03_12_103117_create_try_images_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('try_images', function (Blueprint $table) {
             $table->id();
-            
+            $table->string('image');
             $table->timestamps();
         });
     }
