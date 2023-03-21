0bc280e5c245c70c5b032064f843b8acadfd2b18
13shinharaguchi <shin13usa@icloud.com>
2023-03-21 07:32:46 +0000

    微調整

--- resources/views/table_of_contents/index.blade.php
+++ resources/views/table_of_contents/index.blade.php
@@ -1,5 +1,16 @@
-<h1>ここに結果をだす</h1>
-<p>{{$user -> name}}</p>
-<p>{{$image1 -> image}}</p>
-<p>{{$image2 -> image}}</p>
-<p>{{$image3 -> image}}</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>raise</title>
+</head>
+<body>
+    <h1>ここに結果をだす</h1>
+    <p>{{$user -> name}}</p>
+    <p>{{$image1 -> image}}</p>
+    <p>{{$image2 -> image}}</p>
+    <p>{{$image3 -> image}}</p>
+</body>
+</html>
