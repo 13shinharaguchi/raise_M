1da5a5e915914b365cf5edb7977640724699d688
13shinharaguchi <shin13usa@icloud.com>
2023-04-04 16:38:37 +0000

    css調整

--- resources/views/table_of_contents/show_all.blade.php
+++ resources/views/table_of_contents/show_all.blade.php
@@ -11,50 +11,65 @@
     <title>raise</title>
 </head>
 <body>
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-green-100">
-　<div class="p-12 mx-auto bg-white shadow-md overflow-hidden sm:rounded-lg" style="width: 700px;">
-  <div class="mb-8">
-    <h1 class="text-xl font-bold">{{$question1}}</h1>
-    <div class="pl-5 flex flex-col">
-      <div class="mb-4 mt-2 flex flex-col">
-        <p class="font-bold">{{$user}}</p>
-        <p>{{$image1}}</p>
-      </div>
-      <div class="flex flex-col">
-        <p class="font-bold">{{$partner_user}}</p>
-        <p>{{$p_image1}}</p>
-      </div>
-    </div>
-  </div>
-  <div class="mb-8">
-    <h1 class="text-xl font-bold">{{$question2}}</h1>
-    <div class="pl-5 flex flex-col">
-      <div class="mb-4 mt-2 flex flex-col">
-        <p class="font-bold">{{$user}}</p>
-        <p>{{$image2}}</p>
-      </div>
-      <div class="mb-4 md:mb-0 flex flex-col">
-        <p class="font-bold">{{$partner_user}}</p>
-        <p>{{$p_image2}}</p>
+<div class="min-h-screen flex flex-col justify-center items-center bg-green-100">
+  <div class="w-full max-w-3xl">
+    <div class="mb-8 ml-3 mr-3 shadow">
+      <div class="bg-gray-100 p-2 rounded-lg">
+        <div class="mb-4">
+          <h1 class="text-lg font-bold ">{{$question1}}</h1>
+        </div>
+        <div class="mb-4 md:mb-0 mt-2 md:mt-0 flex flex-col md:flex-row justify-between">
+          <div class="mb-4 md:mb-0 mr-0 md:mr-4">
+            <p class="font-bold">{{$user}}</p>
+            <p>{{$image1}}</p>
+          </div>
+          <div class="flex flex-col">
+            <p class="font-bold">{{$partner_user}}</p>
+            <p>{{$p_image1}}</p>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="mb-8">
-    <h1 class="text-xl font-bold">{{$question3}}</h1>
-    <div class="pl-5 flex flex-col">
-      <div class="mb-4 mt-2 flex flex-col">
-        <p class="font-bold">{{$user}}</p>
-        <p>{{$image3}}</p>
-      </div>
-      <div class="mb-4 md:mb-0 flex flex-col">
-        <p class="font-bold">{{$partner_user}}</p>
-        <p>{{$p_image3}}</p>
+
+    <div class="mb-8 ml-3 mr-3 shadow">
+      <div class="bg-gray-100 p-2 rounded-lg">
+        <div class="mb-4">
+          <h1 class="text-lg font-bold ">{{$question2}}</h1>
+        </div>
+        <div class="mb-4 md:mb-0 mt-2 md:mt-0 flex flex-col md:flex-row justify-between">
+          <div class="mb-4 md:mb-0 mr-0 md:mr-4">
+            <p class="font-bold">{{$user}}</p>
+            <p>{{$image2}}</p>
+          </div>
+          <div class="flex flex-col">
+            <p class="font-bold">{{$partner_user}}</p>
+            <p>{{$p_image2}}</p>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="flex justify-center">
-    <a href="{{ URL::previous() }}" class="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2">戻る</a>
-  </div>
+
+    <div class="mb-8 ml-3 mr-3 shadow">
+      <div class="bg-gray-100 p-2 rounded-lg">
+        <div class="mb-4">
+          <h1 class="text-lg font-bold ">{{$question3}}</h1>
+        </div>
+        <div class="mb-4 md:mb-0 mt-2 md:mt-0 flex flex-col md:flex-row justify-between">
+          <div class="mb-4 md:mb-0 mr-0 md:mr-4">
+            <p class="font-bold">{{$user}}</p>
+            <p>{{$image3}}</p>
+          </div>
+          <div class="flex flex-col">
+            <p class="font-bold">{{$partner_user}}</p>
+            <p>{{$p_image3}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex justify-center w-full">
+      <a href="{{ URL::previous() }}" class="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2">戻る</a>
+    </div>
   </div>
 </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,13 +13,14 @@
 </head>
 <body>
 <div class="min-h-screen flex flex-col justify-center items-center bg-green-100">
+  <form action="{{ route('user_register.create') }}" method="GET">
+    <button class="bg-blue-500 hover:bg-blue-700 text-white px-2 py-1 mb-10">始める</button>
+  </form>
   <div class="flex justify-center">
     <a href="{{ route('how_to_use') }}" class="bg-blue-500 hover:bg-blue-700 text-white px-2 py-1">使い方</a>
   </div>
-  <form action="{{ route('user_register.create') }}" method="GET">
-    <button class="bg-blue-500 hover:bg-blue-700 text-white px-2 py-1">始める</button>
-  </form>
 </div>
+
 
 </body>
 </html>
