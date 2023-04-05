3c4dcc81a790b95ece57399808e354e354996057
13shinharaguchi <shin13usa@icloud.com>
2023-04-05 21:52:18 +0000

    css調整

--- resources/views/try_image/partner_create_g3.blade.php
+++ resources/views/try_image/partner_create_g3.blade.php
@@ -15,7 +15,7 @@
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-12 sm:pt-0 bg-green-100">
     <div class="p-4 sm:p-12 mx-auto bg-white shadow-md overflow-hidden sm:rounded-lg w-full sm:w-2/3 lg:w-1/2">
         <div class="text-center">{{$wanna_try}}</div>
-        <div class="text-center">にとって</div>
+        <div class="text-center">に</div>
         <div class="text-center">{{$question3}}</div>
         <form action="{{ route('partner_image.subg3_store') }}" method="POST" class="mt-4">
             @csrf
